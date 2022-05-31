f67d2d4ab20110d4e0ddca9a2ff85021a20efee9
lillycoroline <corolinej@gmail.com>
2022-05-30 19:29:26 -0500

    First Commit

---
+++ src/main/java/JavaProgram/VowelsSubstring.java
@@ -0,0 +1,58 @@
+package JavaProgram;
+
+public class VowelsSubstring {
+	
+
+
+	
+		public static void main(String[] args) {
+			String x="AUTOMATION TESTING'S PERFORMANCE ACTIVITY";
+			//char y[]=x.toCharArray();
+			//int size=y.length;
+			int size=x.length();
+			int vowelcount=0;
+			int constantcount=0;
+			int specialcount=0;
+			
+			int i=0;
+			while(i!=size)
+			{
+				if(x.charAt(i)>='A' && x.charAt(i)<='Z' )
+				{
+					if (x.charAt(i)=='A'||x.charAt(i)=='E'||x.charAt(i)=='I'||x.charAt(i)=='O'||x.charAt(i)=='U')
+					{
+						++vowelcount;
+					}
+				
+				else 
+				{
+					++constantcount;
+				}
+				}
+			      else
+			
+			    	  
+			{
+				
+				++specialcount;
+			}
+			++i;
+			
+				}
+				System.out.println(x.length());
+				System.out.println("Vowelcount :" +vowelcount);
+				System.out.println("Consonantcount :" +constantcount);
+				System.out.println("Specialcount :" +specialcount);
+
+	}
+}
+	
+	/*
+	 * RESULT; AUTOMATION TESTING'S PERFORMANCE ACTIVITY
+	 *  Vowelcount :15
+	 * Consonantcount :22 
+	 * Specialcount :4
+	 */
+		
+		
+		
